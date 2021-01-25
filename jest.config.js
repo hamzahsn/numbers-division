db56ed8a8305cd198ce7e1addf8e60c1b77a317f
hamzahsn <amdouni.hamza@hotmail.fr>
2021-01-25 14:26:32 +0100

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -9,19 +9,13 @@ module.exports = {
   ],
   coverageDirectory: '<rootDir>/__tests__/__coverage__/',
   moduleDirectories: ['node_modules', 'src'],
-  collectCoverageFrom: [
-    '<rootDir>/**/*.tsx',
-    '<rootDir>/**/**/*.tsx',
-    '!<rootDir>/index.tsx',
-    '!<rootDir>/**/*.d.ts',
-    '!<rootDir>/test/mock/*.(ts|tsx)'
-  ],
+  collectCoverageFrom: ['<rootDir>/**/*.tsx', '<rootDir>/**/**/*.tsx', '!<rootDir>/index.tsx', '!<rootDir>/**/*.d.ts'],
   roots: ['<rootDir>'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/__tests__/config/importJestDOM.ts', '<rootDir>/__tests__/config/setupTest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/__tests__/config/importJestDOM.ts'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__tests__/__mock__/fileMock.ts',
